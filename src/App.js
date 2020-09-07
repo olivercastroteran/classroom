695eb74d13248c179fe34dedba2e27ec7669cf51
olivercastroteran <olivercastroteran@gmail.com>
2020-09-06 22:33:21 -0400

    App structure set up plus react-reouter-dom installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import ClasrroomLogo from './assets/icons/classroom-icon.svg';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <img src={ClasrroomLogo} alt="logo" />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Classroom</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
